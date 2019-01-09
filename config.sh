5d02cdf92067c88df0758f477f96018bca1048e5
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-01-09 16:30:58 +0000

    #CI added config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+function pre_build {
+	:
+}
+
+function pip_opts {
+    # Extra options for pip
+    if [ ! -n "$IS_OSX" ]; then
+        local suffix=manylinux
+    fi
+}
+
+function run_tests {
+}
